65f3cd712718963f45a62872f57be4fcaf2393a2
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-04-20 02:42:29 -0500

    se crea el crud de seccion con su controladores y lauout  con sus temolate

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SectionsController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/test', [HomeController::class, 'test']);
+Route::get('/sections', [SectionsController::class, 'index'])->name('sections.index');
+Route::get('/sections/create', [SectionsController::class, 'create'])->name('sections.create');
+Route::get('/sections/edit/{id}', [SectionsController::class, 'edit'])->name('sections.edit');
+
+Route::post('/sections/store', [SectionsController::class, 'store'])->name('sections.store');
+Route::put('/sections/update', [SectionsController::class, 'update'])->name('sections.update');
+Route::delete('/sections/delete/{id}', [SectionsController::class, 'delete'])->name('sections.delete');
