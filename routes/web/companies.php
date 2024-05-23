8f1109a0d7026ce3c0ee2fbab2f97c74aa131c7d
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-05-22 23:56:41 -0500

    se modifican rutas con su middleware y se crea nuevas rutas

--- routes/web/companies.php
+++ routes/web/companies.php
@@ -1,18 +1,33 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\RolesController;
+use App\Http\Middleware\AuthorizedMiddleware;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 
 
+Route::get('/companies', [CompanyController::class, 'index'])
+->name('companies.index')
+->middleware(AuthorizedMiddleware::class . ':Companies.showCompanies');
 
-Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
-Route::get('/companies/create', [CompanyController::class, 'create'])->name('companies.create');
-Route::get('/companies/edit/{id}', [CompanyController::class, 'edit'])->name('companies.edit');
+Route::get('/companies/create', [CompanyController::class, 'create'])
+->name('companies.create')
+->middleware(AuthorizedMiddleware::class . ':Companies.creteCompanies');
 
-Route::post('/companies/store', [CompanyController::class, 'store'])->name('companies.store');
-Route::put('/companies/update', [CompanyController::class, 'update'])->name('companies.update');
-Route::delete('/companies/delete/{id}', [CompanyController::class, 'delete'])->name('companies.delete');
+Route::get('/companies/edit/{id}', [CompanyController::class, 'edit'])
+->name('companies.edit')
+->middleware(AuthorizedMiddleware::class . ':Companies.updateCompanies');
+
+Route::post('/companies/store', [CompanyController::class, 'store'])
+->name('companies.store')
+->middleware(AuthorizedMiddleware::class . ':Companies.creteCompanies');
+
+Route::put('/companies/update', [CompanyController::class, 'update'])
+->name('companies.update')
+->middleware(AuthorizedMiddleware::class . ':Companies.updateCompanies');
+
+Route::delete('/companies/delete/{id}', [CompanyController::class, 'delete'])
+->name('companies.delete')
+->middleware(AuthorizedMiddleware::class . ':Companies.deleteCpmanies');
+
 
