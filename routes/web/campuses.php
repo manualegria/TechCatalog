c904d57a1e0353c61f6fa84fb47c86b8f3fbbd81
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-08-17 20:59:07 -0500

    "refactorizacin"

--- routes/web/campuses.php
+++ routes/web/campuses.php
@@ -5,27 +5,27 @@ use App\Http\Middleware\AuthorizedMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/campus', [CampusController::class, 'index'])
+Route::get('/campuses', [CampusController::class, 'index'])
      ->name('campus.index')
-     ->middleware(AuthorizedMiddleware::class . ':Campuses.showCampueses');
+     ->middleware(AuthorizedMiddleware::class . ':Sedes.showCampuses');
 
 Route::get('/campuses/create', [CampusController::class, 'create'])
      ->name('campus.create')
-     ->middleware(AuthorizedMiddleware::class . ':Campuses.createCampueses');
+     ->middleware(AuthorizedMiddleware::class . ':Sedes.createCampuses');
 
 Route::get('/campuses/edit/{id}', [CampusController::class, 'edit'])
      ->name('campus.edit')
-     ->middleware(AuthorizedMiddleware::class . ':Campuses.updateCampueses');
+     ->middleware(AuthorizedMiddleware::class . ':Sedes.updateCampuses');
 
 Route::post('/campuses/store', [CampusController::class, 'store'])
      ->name('campus.store')
-     ->middleware(AuthorizedMiddleware::class . ':Campuses.createCampueses');
+     ->middleware(AuthorizedMiddleware::class . ':Sedes.createCampuses');
 
 Route::put('/campuses/update', [CampusController::class, 'update'])
      ->name('campus.update')
-     ->middleware(AuthorizedMiddleware::class, ':Campuses.updateCampueses');
+     ->middleware(AuthorizedMiddleware::class . ':Sedes.updateCampuses');
 
      Route::delete('/campuses/delete/{id}', [CampusController::class, 'delete'])
 ->name('campus.delete')
-->middleware(AuthorizedMiddleware::class . ':campuses.deleteCampuses');
+->middleware(AuthorizedMiddleware::class . ':Sedes.deleteCampuses');
 
--- routes/web/companies.php
+++ routes/web/companies.php
@@ -8,26 +8,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/companies', [CompanyController::class, 'index'])
 ->name('companies.index')
-->middleware(AuthorizedMiddleware::class . ':Companies.showCompanies');
+->middleware(AuthorizedMiddleware::class . ':Empresas.showCompanies');
 
 Route::get('/companies/create', [CompanyController::class, 'create'])
 ->name('companies.create')
-->middleware(AuthorizedMiddleware::class . ':Companies.creteCompanies');
+->middleware(AuthorizedMiddleware::class . ':Empresas.creteCompanies');
 
 Route::get('/companies/edit/{id}', [CompanyController::class, 'edit'])
 ->name('companies.edit')
-->middleware(AuthorizedMiddleware::class . ':Companies.updateCompanies');
+->middleware(AuthorizedMiddleware::class . ':Empresas.updateCompanies');
 
 Route::post('/companies/store', [CompanyController::class, 'store'])
 ->name('companies.store')
-->middleware(AuthorizedMiddleware::class . ':Companies.creteCompanies');
+->middleware(AuthorizedMiddleware::class . ':Empresas.creteCompanies');
 
 Route::put('/companies/update', [CompanyController::class, 'update'])
 ->name('companies.update')
-->middleware(AuthorizedMiddleware::class . ':Companies.updateCompanies');
+->middleware(AuthorizedMiddleware::class . ':Empresas.updateCompanies');
 
 Route::delete('/companies/delete/{id}', [CompanyController::class, 'delete'])
 ->name('companies.delete')
-->middleware(AuthorizedMiddleware::class . ':Companies.deleteCpmanies');
+->middleware(AuthorizedMiddleware::class . ':Empresas.deleteComanies');
 
 
