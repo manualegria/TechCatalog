a06b3fbf37a36dc3ffbede2c291c0ded465c5e91
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-08-17 20:57:43 -0500

    "fix: se agrega campo address en el formulario para que se pueda actualizar"

--- resources/views/campus/edit.blade.php
+++ resources/views/campus/edit.blade.php
@@ -24,17 +24,24 @@
                 <form class="row g-3" action="{{ route('campus.update')}}" method="POST">
                     @csrf
                     @method('PUT')
-                    <input type="hidden" name="campus_id" value="{{ $campus->id }}" />
+                    <input type="hidden" name="campus_id" value="{{ $campuses->id }}" />
+                    <input type="hidden" name="campus_id" value="{{ $campuses->id }}" />
 
                     <div class="col-md-12">
                         <div class="form-floating">
-                            <input name="name" class="form-control" placeholder="Sede" value="{{ $campus->name }}">
+                            <input name="name" class="form-control" placeholder="Sede" value="{{ $campuses->name }}">
                             <label>Sede</label>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-floating">
+                            <input name="address" class="form-control" placeholder="Sede" value="{{ $campuses->address }}">
+                            <label>Direccion</label>
                         </div>
                     </div>
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary">Guardar</button>
-                        <a href="{{ route('campuses.index')}}" class="btn btn-secondary">Volver</a>
+                        <a href="{{ route('campus.index')}}" class="btn btn-secondary">Volver</a>
                     </div>
                 </form>
 
